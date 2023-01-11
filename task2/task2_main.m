4753d250214fe3455016ffaa8c62d2f50e096ada
João Da Fonseca <84569788+JoaoDaFonsecaMC@users.noreply.github.com>
2023-01-11 23:14:34 +0000

    task2

--- task2/task2_main.m
+++ task2/task2_main.m
@@ -9,12 +9,32 @@ t_f = 1;
 
 %time step
 h = (t_f-t_0)/N;
-
 time = t_0:h:t_f;
 
+%initial guess
 X0 = ones(N+1,5);
 
+%minimization
 options=optimoptions('fmincon','MaxFunctionEvaluations',10000000000000,'MaxIterations',10000000000000);
 [X,fval,exitflag,output] = fmincon(@cost_function,X0,[],[],[],[],[],[],@restrictions,options);
 
+%plots
+figure()
 plot(time,X(:,1));
+title("Phi vs time");
+
+figure()
+plot(time,X(:,2));
+title("u vs time");
+
+figure()
+plot(time,X(:,3));
+title("v vs time");
+
+figure()
+plot(time,X(:,4));
+title("r vs time");
+
+figure()
+plot(time,X(:,5));
+title("theta vs time");
