fd16e431a0a04eec608a066591ee50658eb7bec2
alessiatagliati <alessiatagliati@yahoo.com>
2023-01-12 18:13:30 +0000

    c

--- task1/confun1.m
+++ task1/confun1.m
@@ -2,6 +2,6 @@ function [c, ceq] = confun1(x)
 % Nonlinear inequality constraints:
 c = [-8+x(1)-x(2)+x(3)-x(4)+x(1)^(2)+x(2)^(2)+x(3)^(2)+x(4)^(2); 
  -10-x(1)-x(4)+x(1)^(2)+2*x(2)^(2)+x(3)^(2)+2*x(4)^(2)];
-% no nonlinear equality constraints:
+%  nonlinear equality constraints:
 ceq = [5-2*x(1)+x(2)+x(4)-2*x(1)^(2)-x(2)^(2)-x(3)^(2)];
 end
