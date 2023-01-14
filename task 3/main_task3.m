c63afc299804265c2e575f6f2885e6175d3186d1
alessiatagliati <alessiatagliati@yahoo.com>
2023-01-14 19:49:55 +0000

    c

--- task 3/main_task3.m
+++ task 3/main_task3.m
@@ -26,39 +26,39 @@ R=mu_r*eye(3);
 
 %Waypoints coordinates and times
 
-Way7= [39 49 25.71 ; 7 29 35];
-Way8= [39 50 34.82 ; 7 29 37];
-Way9= [39 51 33.38 ; 7 29 39];
-Way10=[39 52 39.86 ; 7 29 41];
-H7=400;
-H8=500;
-H9=600;
-H10=600;
-t7=0*3600; %times from hours to seconds
-t8=0.035*3600;
-t9=0.070*3600;
-t10=0.080*3600;
+Way1= [39 49 25.71 ; 7 29 35];
+Way2= [39 50 34.82 ; 7 29 37];
+Way3= [39 51 33.38 ; 7 29 39];
+Way4=[39 52 39.86 ; 7 29 41];
+H1=400;
+H2=500;
+H3=600;
+H4=600;
+t1=0*3600; %times from hours to seconds
+t2=0.035*3600;
+t3=0.070*3600;
+t4=0.080*3600;
 
 %Waypoints in degress
-Way7_deg=dms2degrees(Way7); % you need Mapping Toolbox to run it 
-Way8_deg=dms2degrees(Way8);
-Way9_deg=dms2degrees(Way9);
-Way10_deg=dms2degrees(Way10);
+Way1_deg=dms2degrees(Way1); % you need Mapping Toolbox to run it 
+Way2_deg=dms2degrees(Way2);
+Way3_deg=dms2degrees(Way3);
+Way4_deg=dms2degrees(Way4);
 
 %Transform West with minus symbol
-Way7_deg(2)=-Way7_deg(2);
-Way8_deg(2)=-Way8_deg(2);
-Way9_deg(2)=-Way9_deg(2);
-Way10_deg(2)=-Way10_deg(2);
+Way1_deg(2)=-Way1_deg(2);
+Way2_deg(2)=-Way2_deg(2);
+Way3_deg(2)=-Way3_deg(2);
+Way4_deg(2)=-Way4_deg(2);
 
 %Waypoints in geocentric coordinates 
-[X7,Y7,Z7]= geodetic_to_geocentric (Way7_deg(2),Way7_deg(1),H7);
-[X8,Y8,Z8]= geodetic_to_geocentric (Way8_deg(2),Way8_deg(1),H8);
-[X9,Y9,Z9]= geodetic_to_geocentric (Way9_deg(2),Way9_deg(1),H9);
-[X10,Y10,Z10]= geodetic_to_geocentric (Way10_deg(2),Way10_deg(1),H10);
+[X1,Y1,Z1]= geodetic_to_geocentric (Way1_deg(2),Way1_deg(1),H1);
+[X2,Y2,Z2]= geodetic_to_geocentric (Way2_deg(2),Way2_deg(1),H2);
+[X3,Y3,Z3]= geodetic_to_geocentric (Way3_deg(2),Way3_deg(1),H3);
+[X4,Y4,Z4]= geodetic_to_geocentric (Way4_deg(2),Way4_deg(1),H4);
 
 %Linear regression to build Yref
-t=t7;
+t=t1;
 step=0.01;
 y_reftotal=[];
 Matrix1=[A B; C zeros(3)];
@@ -66,33 +66,33 @@ Matrix2=[zeros(6,3); eye(3)];
 Asterix_matrix=[];
 j=1;
 
-while(t<t8)
- Y_refnew7= [X7+(X8-X7)/(t8-t7)*(t-t7);
-             Y7+(Y8-Y7)/(t8-t7)*(t-t7);
-             Z7+(Z8-Z7)/(t8-t7)*(t-t7)];
- y_reftotal=cat(2,y_reftotal,Y_refnew7); % cat concanates arrays
+while(t<t2)
+ Y_refnew1= [X1+(X2-X1)/(t2-t1)*(t-t1);
+             Y1+(Y2-Y1)/(t2-t1)*(t-t1);
+             Z1+(Z2-Z1)/(t2-t1)*(t-t1)];
+ y_reftotal=cat(2,y_reftotal,Y_refnew1); % cat concanates arrays
  New_asterix=inv(Matrix1)*Matrix2*y_reftotal(:,j);
  Asterix_matrix=cat(2,Asterix_matrix,New_asterix);
  t=t+step;
  j=j+1;
 end
 
-while(t<t9)
- Y_refnew8=[X8+(X9-X8)/(t9-t8)*(t-t8);
-             Y8+(Y9-Y8)/(t9-t8)*(t-t8);
-             Z8+(Z9-Z8)/(t9-t8)*(t-t8)];
- y_reftotal=cat(2,y_reftotal,Y_refnew8); % cat concanates arrays
+while(t<t3)
+ Y_refnew2=[X2+(X3-X2)/(t3-t2)*(t-t2);
+             Y2+(Y3-Y2)/(t3-t2)*(t-t2);
+             Z2+(Z3-Z2)/(t3-t2)*(t-t2)];
+ y_reftotal=cat(2,y_reftotal,Y_refnew2); % cat concanates arrays
  New_asterix=inv(Matrix1)*Matrix2*y_reftotal(:,j);
  Asterix_matrix=cat(2,Asterix_matrix,New_asterix);
  t=t+step;
  j=j+1;
 end
 
-while(t<t10)
-    Y_refnew9=[X9+(X10-X9)/(t10-t9)*(t-t9);
-             Y9+(Y10-Y9)/(t10-t9)*(t-t9);
-             Z9+(Z10-Z9)/(t10-t9)*(t-t9)];
- y_reftotal=cat(2,y_reftotal,Y_refnew9); % cat concanates arrays
+while(t<t4)
+    Y_refnew3=[X3+(X4-X3)/(t4-t3)*(t-t3);
+             Y3+(Y4-Y3)/(t4-t3)*(t-t3);
+             Z3+(Z4-Z3)/(t4-t3)*(t-t3)];
+ y_reftotal=cat(2,y_reftotal,Y_refnew3); % cat concanates arrays
  New_asterix=inv(Matrix1)*Matrix2*y_reftotal(:,j);
  Asterix_matrix=cat(2,Asterix_matrix,New_asterix);
  t=t+step;
@@ -114,8 +114,8 @@ uctotal=[];
 x0=cat(1,y_reftotal(:,1),v_0);
 xk=x0;
 
-t=t7;
-while(t<=t8)
+t=t1;
+while(t<=t2)
  uc_k=inv(w)*(w*uc_ast(:,k)-B'*P*(xk-x_ast(:,k)));
  xk=Ad*xk+Bd*uc_k;
  k=k+1;
@@ -124,7 +124,7 @@ while(t<=t8)
  uctotal=cat(2,uctotal,uc_k);
 end
 
-while(t<=t9)
+while(t<=t3)
  uc_k=inv(w)*(w*uc_ast(:,k)-B'*P*(xk-x_ast(:,k)));
  xk=Ad*xk+Bd*uc_k;
  k=k+1;
@@ -133,7 +133,7 @@ while(t<=t9)
  uctotal=cat(2,uctotal,uc_k);
 end
 
-while(t<=t10)
+while(t<=t4)
  uc_k=inv(w)*(w*uc_ast(:,k)-B'*P*(xk-x_ast(:,k)));
  xk=Ad*xk+Bd*uc_k;
  k=k+1;
@@ -142,42 +142,55 @@ while(t<=t10)
  uctotal=cat(2,uctotal,uc_k);
 end
 
-time=t7:step:t10;
+time=t1:step:t4;
 %Plots
 figure ()
 plot3(xtotal(1,:) , xtotal(2,:) , xtotal(3,:));
 xlabel('X[Km]')
 ylabel('Y[Km]')
 zlabel('Z[Km]')
+title('3D Navigation')
 grid on
 hold on
-plot3(X7 , Y7 , Z7, 'o');
-plot3(X8 , Y8 , Z8, 'o');
-plot3(X9 , Y9 , Z9, 'o');
-plot3(X10 , Y10 , Z10, 'o');
+plot3(X1 , Y1 , Z1, 'o');
+plot3(X2 , Y2 , Z2, 'o');
+plot3(X3 , Y3 , Z3, 'o');
+plot3(X4 , Y4 , Z4, 'o');
 
 figure ()
 plot(time,xtotal(4,:))
+xlabel ('time [s]')
+ylabel ('vx [km/s]')
 title('vx in time')
 
 figure ()
 plot(time,xtotal(5,:))
+xlabel ('time [s]')
+ylabel ('vy [km/s]')
 title('vy in time')
 
 figure ()
 plot(time,xtotal(6,:))
+xlabel ('time [s]')
+ylabel ('vz [km/s]')
 title('vz in time')
 
 figure ()
 plot(time,uctotal(1,:))
+xlabel ('time [s]')
+ylabel ('ax [km/s^2]')
 title('ax in time')
 
 figure ()
 plot(time,uctotal(2,:))
+xlabel ('time [s]')
+ylabel ('ay [km/s^2]')
 title('ay in time')
 
 figure ()
 plot(time,uctotal(3,:))
+xlabel ('time [s]')
+ylabel ('az [km/s^2]')
 title('az in time')
 
 
